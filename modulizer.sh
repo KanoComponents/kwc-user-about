32a1f0c73583d57ed019459cc0ec1407ff970434
Paul Varache <perso@paulvarache.ninja>
2018-06-14 16:12:35 +0100

    3.0.0-beta.3

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --npm-name @kano/kwc-user-about \
---npm-version 3.0.0-beta.2 \
+--npm-version 3.0.0-beta.3 \
 --import-style name \
 --dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.2 \
 --dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.2 \
