4836affc48cdc78522b2b0c2cebefa8361eb1b75
Paul Varache <paul@kano.me>
2018-09-18 17:35:39 +0100

    Remove unused files

--- modulizer.sh
+++
@@ -1,7 +0,0 @@
-modulizer --out . \
---npm-name @kano/kwc-user-about \
---npm-version 3.0.0-beta.5 \
---import-style name \
---dependency-mapping kwc-icons,@kano/kwc-icons,^3.0.0-beta.2 \
---dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2 \
---dependency-mapping kwc-button,@kano/kwc-button,^3.0.0-beta.2
